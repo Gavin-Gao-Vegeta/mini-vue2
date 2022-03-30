6399f73ccd8b355fbe1f475755181766237c7a90
Yuan Gao <gaoy78@cardiff.ac.uk>
2022-03-30 21:59:08 +0800

    first commit

---
+++ bin/bind.js
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @version: 0.0.0
+ * @Author: gavin
+ * @Date: 2022-03-30 21:56:02
+ * @LastEditors: gavin
+ * @LastEditTime: 2022-03-30 21:57:09
+ */
+//响应式数据
+//1.原始方式更新数据
+let a = 10
+let b = a + 5
+console.log(b)
+a = 20
+console.log(b)
